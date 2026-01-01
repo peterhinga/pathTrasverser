e600e516bcbf51d1dfaa2ba077ce4aefcb893012
Peter Hinga <hingapetes6@gmail.com>
2026-01-01 21:37:40 +0300

    Added the ascii art,

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    pass
+from evasions import encoding
+import pyfiglet
 
+ascii_banner = pyfiglet.figlet_format("Path Trasverser")
+print(ascii_banner)
+def main():
+    encoding.url_encoding(link, payload)
+    encoding.unicode_encoding(link, payload)
 if __name__ in "__main__":
-    link = input("input the website link:")
+    link =  input("input the website link:")
+    payload = input("Enter the file path you are looking for:")
     main = main()
